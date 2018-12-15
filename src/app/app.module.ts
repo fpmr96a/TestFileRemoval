72c9fb1479e6df69cbdc6537b9ca5e46445d5d9b
fpmr96a <softwaresolutions@cox.net>
2018-12-15 15:44:11 -0500

    initial version

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ProductListComponent} from './products/product-list.component';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
